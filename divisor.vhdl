1b3874b185d7ea084bc28fec479cc34ca72c894b
Feanor19 <53187755+YuriRB@users.noreply.github.com>
2021-05-06 13:30:27 -0300

    Update divisor.vhdl

--- divisor.vhdl
+++ divisor.vhdl
@@ -2,7 +2,7 @@ ENTITY divisor IS
 	PORT (
 		A   : IN bit_VECTOR(11 DOWNTO 0); 
 		B   : IN bit_VECTOR(4 DOWNTO 0); 
-		S   : OUT bit_VECTOR(3 DOWNTO 0)
+		S   : OUT bit_VECTOR(11 DOWNTO 0)
 	);
 END ENTITY;
 
@@ -25,9 +25,9 @@ ARCHITECTURE Comportamento_Divisor OF divisor IS
 	
 	COMPONENT bloco_somador IS
 		PORT(	
-			A   : IN BIT_VECTOR(3 DOWNTO 0); 
+			A   : IN BIT_VECTOR(11 DOWNTO 0); 
 			B   : IN BIT; 
-			S   : OUT BIT_VECTOR(3 DOWNTO 0)
+			S   : OUT BIT_VECTOR(11 DOWNTO 0)
 		);
 	END COMPONENT;
 	
@@ -39,42 +39,10 @@ ARCHITECTURE Comportamento_Divisor OF divisor IS
 BEGIN
 	Resto <= A;
 	A_Somador <= "0000";
-	Comp1: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub1: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som1: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp2: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub2: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som2: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp3: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub3: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som3: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp4: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub4: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som4: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp5: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub5: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som5: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp6: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub6: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som6: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp7: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub7: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som7: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp8: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub8: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som8: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp9: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub9: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som9: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp10: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub10: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som10: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp11: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub11: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som11: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);
-	Comp12: bloco_comparador PORT MAP (Resto,B,S_Mag);
-	Sub12: bloco_subtrator PORT MAP (Resto,B,Resto);
-	Som12: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);	
+	Comp: bloco_comparador PORT MAP (Resto,B,S_Mag);
+	Sub: bloco_subtrator PORT MAP (Resto,B,Resto);
+	Soma: bloco_somador PORT MAP(A_Somador,S_Mag,Resultado1);	
 		
 END ARCHITECTURE;
+
 
