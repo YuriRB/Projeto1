d2c762aa292fa129c500f5dc26e716c04310c931
Feanor19 <53187755+YuriRB@users.noreply.github.com>
2021-05-06 09:10:24 -0300

    Update divisor.vhdl

--- divisor.vhdl
+++ divisor.vhdl
@@ -1,10 +1,25 @@
-ENTITY Subtrator IS
-	PORT (A,B,Tin: IN bit; S,Tout: OUT bit);
+ENTITY divisor IS
+	PORT (A,B: IN bit; S: OUT bit);
 END ENTITY;
 
-ARCHITECTURE Comportamento_Subtrator OF Subtrator IS
+ARCHITECTURE Comportamento_Divisor OF divisor IS
+	COMPONENT bloco_comparador IS
+		PORT(
+			A: IN BIT_VECTOR(11 DOWNTO 0); 
+			B: IN BIT_VECTOR(4 DOWNTO 0); 
+			S_Maior_Igual: OUT BIT
+		);
+	END COMPONENT;
+	COMPONENT bloco_subtrator IS
+		PORT(
+			A   : IN bit_VECTOR(11 DOWNTO 0); 
+			B   : IN bit_VECTOR(4 DOWNTO 0); 
+			S   : OUT bit_VECTOR(11 DOWNTO 0)
+		);
+	END COMPONENT;
 BEGIN
-	S <= ((A) XOR ((B) XOR (Tin)));
-	Tout <= (((not A) AND (B)) OR ((not A) AND (Tin)) OR ((B) AND (Tin)));
+	WHEN 
+		
 END ARCHITECTURE;
+
 
